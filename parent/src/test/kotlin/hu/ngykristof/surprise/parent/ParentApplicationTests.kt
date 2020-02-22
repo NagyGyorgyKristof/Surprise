461688f0f81a795e25e3937a554f99eb4569a217
Kristof Nagy-Gyorgy <nagygyorgykristof@gmail.com>
2020-02-22 14:51:58 +0100

    Init the project

---
+++ parent/src/test/kotlin/hu/ngykristof/surprise/parent/ParentApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.ngykristof.surprise.parent
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ParentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

d3ff47a327a7c3e91d9a6deaa8559f85334b63dc
Kristof Nagy-Gyorgy <nagygyorgykristof@gmail.com>
2020-02-22 15:41:31 +0100

    Create service discovery

---
+++ parent/service-discovery/src/main/kotlin/hu/ngykristof/surprise/servicediscovery/ServiceDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package hu.ngykristof.surprise.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer    // Enable eureka server
+public class ServiceDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDiscoveryApplication.class, args);
+    }
+
+}

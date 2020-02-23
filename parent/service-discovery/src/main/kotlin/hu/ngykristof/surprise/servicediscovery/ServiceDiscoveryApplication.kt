b69736093c8e0299f6ef6ee7bb33c379ef3ca52d
Kristof Nagy-Gyorgy <nagygyorgykristof@gmail.com>
2020-02-23 16:32:14 +0100

    Recreate the service discovery service with kotlin support

---
+++ parent/service-discovery/src/main/kotlin/hu/ngykristof/surprise/servicediscovery/ServiceDiscoveryApplication.kt
@@ -0,0 +1,13 @@
+package hu.ngykristof.surprise.servicediscovery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@SpringBootApplication
+@EnableEurekaServer
+class ServiceDiscoveryApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServiceDiscoveryApplication>(*args)
+}
---
+++ parent/service-discovery/src/test/kotlin/hu/ngykristof/surprise/servicediscovery/ServiceDiscoveryApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.ngykristof.surprise.servicediscovery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

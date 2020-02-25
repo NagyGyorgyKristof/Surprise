46efc87a860f6f616b59693a979aef227a5cad0f
Nagy-Gyorgy Kristof <nagygyorgy.kristof@autsoft.hu>
2020-02-25 16:28:05 +0100

    Enable eureka client in the gateway service

--- parent/gateway/src/main/kotlin/hu/ngykristof/surprise/gateway/GatewayApplication.kt
+++ parent/gateway/src/main/kotlin/hu/ngykristof/surprise/gateway/GatewayApplication.kt
@@ -2,8 +2,11 @@ package hu.ngykristof.surprise.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class GatewayApplication
 
 fun main(args: Array<String>) {

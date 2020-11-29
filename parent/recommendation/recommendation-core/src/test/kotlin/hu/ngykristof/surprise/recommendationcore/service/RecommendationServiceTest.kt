40aacd6f3037953b8e42a2a626a20fa29e1b20ee
Kristof Nagy-Gyorgy <nagygyorgykristof@gmail.com>
2020-11-29 17:45:22 +0100

    Outsource recommendation mock data

--- parent/recommendation/recommendation-core/src/test/kotlin/hu/ngykristof/surprise/recommendationcore/service/RecommendationServiceTest.kt
+++ parent/recommendation/recommendation-core/src/test/kotlin/hu/ngykristof/surprise/recommendationcore/service/RecommendationServiceTest.kt
@@ -5,6 +5,9 @@ import hu.ngykristof.surprise.recommendationcore.data.PersonalRecommendationEnti
 import hu.ngykristof.surprise.recommendationcore.repository.RecommendationRepository
 import hu.ngykristof.surprise.recommendationcore.service.message.CreateRatingMessage
 import hu.ngykristof.surprise.recommendationcore.service.message.toMessage
+import hu.ngykristof.surprise.recommendationcore.service.result.toResponse
+import hu.ngykristof.surprise.recommendationcore.test.createMockCreateRatingMessage
+import hu.ngykristof.surprise.recommendationcore.test.createMockPersonalRecommendationEntity
 import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.mockk
@@ -27,7 +30,7 @@ internal class RecommendationServiceTest {
     @Test
     fun getUserBasedRecommendation() {
         val userId = "userId"
-        val recommendationEntity = PersonalRecommendationEntity()
+        val recommendationEntity = createMockPersonalRecommendationEntity()
 
         every { recommendationRepository.userBasedRecommendation(userId) } returns listOf(recommendationEntity)
 
@@ -44,35 +47,35 @@ internal class RecommendationServiceTest {
     @Test
     fun getContentBasedRecommendation() {
         val userId = "userId"
-        val recommendationEntity = PersonalRecommendationEntity()
+        val recommendationEntity = createMockPersonalRecommendationEntity()
 
         every { recommendationRepository.contentBasedRecommendation(userId) } returns listOf(recommendationEntity)
 
-        val userBasedRecommendation = recommendationService.getContentBasedRecommendation(userId)
+        val contentBasedRecommendation = recommendationService.getContentBasedRecommendation(userId)
 
         verify { recommendationRepository.contentBasedRecommendation(userId) }
         confirmVerified(recommendationRepository)
 
-        assertEquals(userBasedRecommendation.first().movieId, recommendationEntity.movieId)
-        assertEquals(userBasedRecommendation.first().ratingMean, recommendationEntity.ratingMean)
-        assertEquals(userBasedRecommendation.first().title, recommendationEntity.title)
+        assertEquals(contentBasedRecommendation.first().movieId, recommendationEntity.movieId)
+        assertEquals(contentBasedRecommendation.first().ratingMean, recommendationEntity.ratingMean)
+        assertEquals(contentBasedRecommendation.first().title, recommendationEntity.title)
     }
 
     @Test
     fun getHybridRecommendation() {
         val userId = "userId"
-        val recommendationEntity = PersonalRecommendationEntity()
+        val recommendationEntity = createMockPersonalRecommendationEntity()
 
         every { recommendationRepository.hybridRecommendation(userId) } returns listOf(recommendationEntity)
 
-        val userBasedRecommendation = recommendationService.getHybridRecommendation(userId)
+        val hybridRecommendation = recommendationService.getHybridRecommendation(userId)
 
         verify { recommendationRepository.hybridRecommendation(userId) }
         confirmVerified(recommendationRepository)
 
-        assertEquals(userBasedRecommendation.first().movieId, recommendationEntity.movieId)
-        assertEquals(userBasedRecommendation.first().ratingMean, recommendationEntity.ratingMean)
-        assertEquals(userBasedRecommendation.first().title, recommendationEntity.title)
+        assertEquals(hybridRecommendation.first().movieId, recommendationEntity.movieId)
+        assertEquals(hybridRecommendation.first().ratingMean, recommendationEntity.ratingMean)
+        assertEquals(hybridRecommendation.first().title, recommendationEntity.title)
     }
 
     @Test
@@ -80,7 +83,7 @@ internal class RecommendationServiceTest {
         val userId = "userId"
         val movieId = "movieId"
         val rating = 5.0
-        val createRatingMessage = CreateRatingMessage(movieId, rating)
+        val createRatingMessage = createMockCreateRatingMessage(movieId, rating)
 
         recommendationService.createRating(userId, createRatingMessage)
 
@@ -99,5 +102,22 @@ internal class RecommendationServiceTest {
 
         verify { recommendationRepository.createRating(userId, movieId, rating) }
         confirmVerified(recommendationRepository)
+    }
+
+    @Test
+    fun recommendationMapping() {
+        val userId = "userId"
+        val recommendationEntity = createMockPersonalRecommendationEntity()
+
+        every { recommendationRepository.userBasedRecommendation(userId) } returns listOf(recommendationEntity)
+
+        val recommendationResponse = recommendationService.getUserBasedRecommendation(userId).map { it.toResponse() }
+
+        verify { recommendationRepository.userBasedRecommendation(userId) }
+        confirmVerified(recommendationRepository)
+
+        assertEquals(recommendationResponse.first().movieId, recommendationEntity.movieId)
+        assertEquals(recommendationResponse.first().ratingMean, recommendationEntity.ratingMean)
+        assertEquals(recommendationResponse.first().title, recommendationEntity.title)
     }
 }

7be301864dba7adf5333c2e0f2857f287389c9f7
Kristof Nagy-Gyorgy <nagygyorgykristof@gmail.com>
2020-12-03 13:58:49 +0100

    Add authorization filter to RecommendationRoutes

--- parent/gateway/src/main/kotlin/hu/ngykristof/surprise/gateway/routes/RecommendationRoutes.kt
+++ parent/gateway/src/main/kotlin/hu/ngykristof/surprise/gateway/routes/RecommendationRoutes.kt
@@ -1,19 +1,30 @@
 package hu.ngykristof.surprise.gateway.routes
 
+import hu.ngykristof.surprise.gateway.filter.AuthenticatedRequestFilter
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.cloud.gateway.route.builder.filters
 import org.springframework.cloud.gateway.route.builder.routes
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.WebClient
 
 
 @Configuration
-class RecommendationRoutes {
+class RecommendationRoutes(
+    private val webClientBuilder: WebClient.Builder,
+    @Value("\${surprise.validate-access-token-url}")
+    private val accessTokenUrl: String
+) {
 
 
     @Bean
     fun recomRouteLocator(builder: RouteLocatorBuilder) = builder.routes {
         route(id = "recom") {
             path("/recommendation/**")
+            filters {
+                this.filter(AuthenticatedRequestFilter(webClientBuilder, accessTokenUrl).apply(Any()))
+            }
             uri("lb://recommendation-service/")
         }
     }
